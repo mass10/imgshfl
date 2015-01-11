c2e37c565af07992c25bcc4047e0edf95570aeb5
mar3 <irisawa.masaru@gmail.com>
2015-01-12 08:15:16 +0900

    wip

---
+++ imgshfl/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imgshfl
+{
+	public sealed class Program
+	{
+		private static void Enum(string path)
+		{
+			foreach (string name in System.IO.Directory.EnumerateFiles(path))
+			{
+				Console.WriteLine(name);
+			}
+			foreach (string name in System.IO.Directory.EnumerateDirectories(path))
+			{
+				//Enum(name);
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			foreach (string path in args)
+			{
+				Enum(path);
+			}
+		}
+	}
+}
